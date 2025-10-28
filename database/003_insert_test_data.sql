d589e77a7f59a285b5103c380f943c0f142df72e
João Arthur <joaoarthurce@gmail.com>
2025-10-27 22:13:03 -0300

    Verificação de testes.

--- database/003_insert_test_data.sql
+++ database/003_insert_test_data.sql
@@ -1,5 +1,5 @@
 DELETE FROM usuarios;
-INSERT INTO usuarios (nome, email, senha, tipo, pontuacao_total) VALUES 
-('Ana', 'ana@email.com', 'senha123', 'aluno', 250),
-('Beto', 'beto@email.com', 'senha123', 'aluno', 500),
-('Carla', 'carla@email.com', 'senha123', 'aluno', 120);
+INSERT INTO usuarios (id, nome, email, senha, tipo, pontuacao_total) VALUES 
+(1, 'Ana', 'ana@email.com', 'senha123', 'aluno', 250),
+(2, 'Beto', 'beto@email.com', 'senha123', 'aluno', 500),
+(3, 'Carla', 'carla@email.com', 'senha123', 'aluno', 120);
